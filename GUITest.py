e7744225dd6440fdd03ede69ac1ab11ad4bb7d5e
RonenGupta <160193990+RonenGupta@users.noreply.github.com>
2025-03-12 00:21:01 +1100

    Fixed some of the discrepancies at home.
    So I noted at school that the button would not open the image in the web browser. I later found out that this was due to the try and except functions, where it would first try the code for printing and opening the URL and furthermore detecting an error. This, for some reason posed an error, especially with the fact that I used the lambda function in the button when I didn't need it, and I also packed the command in the same line possibly making it not work. However, I made my code much more simpler to utilise and have created a basic button to open the APOD in a web browser.

--- GUITest.py
+++ GUITest.py
@@ -5,41 +5,48 @@ from tkinter import ttk
 
 apod = mm.get_apod()
 
-root = tk.Tk()
-root.title("NASA NERD GUIDE")
-root.geometry("900x600")
-root.config(bg="black")
+def GUI():
     
-NASANOTEBOOK = ttk.Notebook(root)
-NASANOTEBOOK.pack(fill='both', expand=True)
+        root = tk.Tk()
+        root.title("NASA NERD GUIDE")
+        root.geometry("900x600")
+        root.config(bg="black")
+    
+        NASANOTEBOOK = ttk.Notebook(root)
+        NASANOTEBOOK.pack(fill='both', expand=True)
 
-makeframe = ttk.Frame(NASANOTEBOOK)
-modelframe = ttk.Frame(NASANOTEBOOK)
-dataframe = ttk.Frame(NASANOTEBOOK)
-helpframe = ttk.Frame(NASANOTEBOOK)
+        makeframe = ttk.Frame(NASANOTEBOOK)
+        modelframe = ttk.Frame(NASANOTEBOOK)
+        dataframe = ttk.Frame(NASANOTEBOOK)
+        helpframe = ttk.Frame(NASANOTEBOOK)
 
-NASANOTEBOOK.add(makeframe, text='Find an APOD!')
-NASANOTEBOOK.add(modelframe, text='Save an APOD!')
-NASANOTEBOOK.add(dataframe, text='View an APOD!')
-NASANOTEBOOK.add(helpframe, text = 'Remove an APOD!')
+        NASANOTEBOOK.add(makeframe, text='Find an APOD!')
+        NASANOTEBOOK.add(modelframe, text='Save an APOD!')
+        NASANOTEBOOK.add(dataframe, text='View an APOD!')
+        NASANOTEBOOK.add(helpframe, text = 'Remove an APOD!')
 
-def ButtonPushed():
-     
-    print(f"\nTitle: {apod['title']}")
-    print(f"Date: {apod['date']}")
-    print(f"Explanation: {apod['explanation']}")
-    print(f"Image URL: {apod['image_url']}")
+        Button = tk.Button(makeframe, text="View the Daily APOD!", command=Find_APOD_Button)
+        Button.pack()
+
+        root.mainloop()
+
+def Find_APOD_Button():
     try:
-            webbrowser.open(apod['image_url'])
+        print(f"\nTitle: {apod['title']}")
+        print(f"Date: {apod['date']}")
+        print(f"Explanation: {apod['explanation']}")
+        print(f"Image URL: {apod['image_url']}")
+        webbrowser.open(apod['image_url'])
     except Exception as e:
-        print(f"Could not open browser: {e}")
+        print(f"Error: {e}")
 
-Button = tk.Button(makeframe, text="View the Daily APOD!", command=lambda: ButtonPushed).pack()
+GUI()
+
 
 
     
 
-root.mainloop()
+
 
 
 
